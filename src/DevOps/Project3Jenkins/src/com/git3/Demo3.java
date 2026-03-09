f6f1eccc4292b4c198b847e3580d58e4ee19d132
Ayon Kumar Saha <112257228+Kumarayoningithub@users.noreply.github.com>
2026-03-09 19:30:23 +0530

    add all Jenkins Project

---
+++ src/DevOps/Project3Jenkins/src/com/git3/Demo3.java
@@ -0,0 +1,15 @@
+package com.git3;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+       
+		 int a = 10;
+	        int b = 20;
+	        int sub = b- a;
+
+	        System.out.println("Subtraction Result: " + sub);
+	}
+
+}
